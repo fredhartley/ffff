97854c515aa30631a4e0a6199379748f119250ff
Fred Hartley <fred@taylorthomas.co.uk>
2024-02-09 08:21:01 +0000

    added complete expand/coll to faq cards

--- scripts.js
+++ scripts.js
@@ -7,8 +7,11 @@ window.addEventListener("DOMContentLoaded", function() {
 
     let faqCards = document.querySelectorAll(".FAQ-card");
     faqCards.forEach(function(card) {
+        let faqCardIcon = card.querySelector(".FAQ-card-icon");
         card.addEventListener("click", function() {
+            faqCardIcon.textContent = faqCardIcon.textContent === "+" ? "-" : "+";
             let content = this.nextElementSibling;
+            card.classList.toggle("active");
             content.style.display = content.style.display === "none" ? "block" : "none";
         });
     });
