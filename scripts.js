c1f8b524e5951f6d3d6dcd21c76fc78bece675d5
Fred Hartley <fred@taylorthomas.co.uk>
2024-02-14 09:28:18 +0000

    further styling

--- scripts.js
+++ scripts.js
@@ -7,54 +7,11 @@ window.addEventListener("DOMContentLoaded", function() {
             faqCardIcon.textContent = faqCardIcon.textContent === "+" ? "-" : "+";
         });
     });
+
+    let faqCardDets = document.querySelectorAll(".FAQ-card");
+    faqCardDets.forEach(function(card) { 
+        card.addEventListener("toggle", () => {
+            card.style.backgroundColor = card.open ? "#8894FF" : "#E0E2F3";
+        });
+    });
 });
-
-// details.addEventListener("toggle", (event) => {
-//     if (details.open) {
-//         faqCardIcon.textContent = "-";
-//       /* the element was toggled open */
-//     } else {
-//         faqCardIcon.textContent = "";
-//       /* the element was toggled closed */
-//     }
-//   });
-
-// window.addEventListener("DOMContentLoaded", function() {
-//     let faqCardContents = document.querySelectorAll(".FAQ-card-content");
-
-//     faqCardContents.forEach(function(content) {
-//         content.style.display = "none";
-//     });
-
-//     let faqCards = document.querySelectorAll(".FAQ-card");
-//     faqCards.forEach(function(card) {
-//         let faqCardIcon = card.querySelector(".FAQ-card-icon");
-//         card.addEventListener("click", function() {
-//             faqCardIcon.textContent = faqCardIcon.textContent === "+" ? "-" : "+";
-//             let content = this.nextElementSibling; // Change this line
-//             card.classList.toggle("active");
-//             content.style.display = content.style.display === "none" ? "block" : "none";
-//         });
-//     });
-// });
-
-
-// window.addEventListener("DOMContentLoaded", function() {
-//     let faqCardContents = document.querySelectorAll(".FAQ-card-content");
-
-//     faqCardContents.forEach(function(content) {
-//         content.style.display = "none";
-//     });
-
-//     let faqCards = document.querySelectorAll(".FAQ-card");
-//     faqCards.forEach(function(card) {
-//         let faqCardIcon = card.querySelector(".FAQ-card-icon");
-//         card.addEventListener("click", function() {
-//             faqCardIcon.textContent = faqCardIcon.textContent === "+" ? "-" : "+";
-//             let content = this.nextElementSibling;
-//             let targetDiv = document.querySelector(".FAQ-card-content"); // Replace ".your-class-name" with your specific class name
-//             card.classList.toggle("active");
-//             content.style.display = content.style.display === "none" ? "block" : "none";
-//         });
-//     });
-// });
